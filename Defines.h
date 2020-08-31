e63c33037065b0b034df40ef21decbf496d4a1b2
Jason Cole <jac256@pitt.edu>
2020-08-31 10:42:06 -0400

    added NUMs
    added NUM counters for number of options in the enumerations

--- Defines.h
+++ Defines.h
@@ -28,8 +28,10 @@
 #define BUTTON_3_PIN              51
 #define BUTTON_4_PIN              52
 
+#define NUM_ANIMATIONS            6  //set to match below enum
 enum ANIMATION_STATE { FIRE, GREEN_RAIN, RAINBOW_CYCLE, RAINBOW_RAIN, TWINKLE, SOLID_FILL };
 
+#define NUM_BASIC_COLORS          8 
 enum BASIC_COLORS    { OFF,     //000000
                        RED,     //FF0000
                        GREEN,   //00FF00
